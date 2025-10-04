3da46946b15fbb434a6670aa893b3b1702fcfd4b
Majd Assad <majdassad123@gmail.com>
2025-10-04 13:30:45 -0400

    Changed the index to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ import Services from "@/components/Services";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
-const Index = () => {
+const HomePage = () => {
   return (
     <div className="min-h-screen">
       <Navigation />
@@ -20,4 +20,4 @@ const Index = () => {
   );
 };
 
-export default Index;
+export default HomePage;
