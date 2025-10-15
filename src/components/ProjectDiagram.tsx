0ab71a913ca56ed674cb5e8c03b7d7de57b9a008
Majd Assad <majdassad123@gmail.com>
2025-10-15 13:13:51 -0400

    Added the content for the story and stone project

--- src/components/ProjectDiagram.tsx
+++ src/components/ProjectDiagram.tsx
@@ -6,11 +6,11 @@ interface ProjectDiagramProps {
 
 const ProjectDiagram: React.FC<ProjectDiagramProps> = ({ concepts }) => {
   return (
-    <div className="flex items-center justify-center gap-8 flex-wrap max-w-lg mx-auto pt-12">
+    <div className="flex items-center justify-center gap-8 flex-wrap max-w-lg pt-12">
       {concepts.map((concept, idx) => (
         <div
           key={idx}
-          className="flex items-center justify-center rounded-full shadow-lg w-24 h-24 md:w-32 md:h-32 bg-accent"
+          className="flex items-center justify-center rounded-full shadow-lg w-24 h-24 md:w-32 md:h-32 bg-primary"
         >
           <span className="text-center text-sm md:text-md font-semibold text-white px-2 break-words">
             {concept}
--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -5,6 +5,8 @@ import ProjectNavigation from "@/components/ProjectNavigation";
 import { ArrowLeft } from "lucide-react";
 import { useEffect, useState } from "react";
 import ProjectDiagram from "@/components/ProjectDiagram";
+import research from "@/assets/story-and-stone/research/research.jpeg";
+import timeline from "@/assets/story-and-stone/research/timeline.jpeg";
 
 const ProjectPage = () => {
   React.useEffect(() => {
@@ -49,7 +51,7 @@ const ProjectPage = () => {
   }, []);
 
   return (
-    <div className="flex flex-col bg-background">
+    <div className="flex flex-col bg-white">
       <section>
         <div className="fixed top-3 left-3 z-50 flex justify-center p-2 rounded-full bg-background/95 backdrop-blur-sm shadow-md border border-border/50">
           <button
@@ -86,7 +88,7 @@ const ProjectPage = () => {
       <div className="container mx-auto px-6 space-y-20">
         <div
           id="concept"
-          className="md:grid grid-cols-1 md:grid-cols-2 gap-8 pt-16"
+          className="md:grid grid-cols-1 md:grid-cols-2 justify-items-end gap-8 pt-16"
         >
           <div>
             <h2 className="text-2xl font-bold font-serif mb-4">
@@ -98,19 +100,197 @@ const ProjectPage = () => {
             <ProjectDiagram concepts={project.concepts} />
           </div>
         </div>
-        <div id="research" className="pt-10 max-w-2xl">
+        <div id="research" className="pt-10">
           <h2 className="text-2xl font-bold font-serif mb-4">Research</h2>
-          <p>{project.research}</p>
+          {/* <p>{project.research}</p> */}
+
+          <div className="md:grid md:grid-cols-2 justify-items-end gap-4 space-y-10 md:space-y-0">
+            <div>
+              <p>
+                We chose Nazareth for our project due to our personal
+                connection, its 2,000-year history, cultural diversity, and
+                significance as the childhood home of Jesus. To understand the
+                city, we studied its layout, surrounding towns, roads, and
+                cultural landscape. Focusing on what the word culture contains,
+                we categorized it into{" "}
+                <b>
+                  heritage & traditions, social & family structure, language &
+                  literature, music & dance, and handcrafts & visual arts.
+                </b>{" "}
+                Mapping key cultural sites revealed they are scattered with no
+                unifying element, and the city has only one library, located far
+                from the center. We also observed that most cultural centers
+                emphasized artistry while overlooking historical depth.
+              </p>
+              <p className="mt-4">
+                This encouraged us to examine Nazareth’s timeline from the
+                Ancient Roman period to the present, highlighting architectural
+                features from each era. Field research revealed surviving
+                elements in the city center, particularly underground
+                archaeological sites such as the Basilica of the Annunciation,
+                holy caves, and the ancient bathhouse. Exploring these sites
+                showed us that many were unfamiliar to locals and primarily
+                oriented toward tourists. We chose the Ancient Roman Bathhouse
+                to avoid religious sites and establish a space that reconnects
+                locals with the city’s rich historical layers.
+              </p>
+            </div>
+            <img
+              src={research}
+              className="md:w-3/5 h-auto object-contain aspect-square w-full"
+              alt=""
+            />
+          </div>
+
+          <img src={timeline} className="w-full h-auto object-contain mt-10" />
         </div>
         <div id="design" className="pt-10">
           <h2 className="text-2xl font-bold font-serif mb-4">Design</h2>
-          <p>{project.design}</p>
+          {/* <p>{project.design}</p> */}
+          <div className="md:grid md:grid-cols-2 gap-4 space-y-10 md:space-y-0">
+            <div>
+              <p>
+                We studied the Roman bathhouse’s original function, layout, and
+                sequence of spaces from the changing room through cold,
+                lukewarm, and hot rooms supported by an underfloor heating
+                system. Today, the bathhouse is divided into separate spaces
+                with individual entrances, and the original connections are
+                closed. To understand the history of the building, we collected
+                visual documents from 1890 to the present and analyzed original
+                versus added stones, aiming to preserve the authentic structure
+                while removing later interventions.
+              </p>
+              <p className="mt-4">
+                Our proposal envisions a public library that reconnects the
+                people of Nazareth with their cultural and historical heritage.
+                We sought to preserve the bathhouse’s original spatial logic,
+                the sequence of hot, lukewarm, and cold rooms, and reimagined it
+                as a narrative structure within the new program.
+              </p>
+            </div>
+            <img
+              src={project.designs[0]}
+              className="oject-cover rounded-lg mx-auto"
+            />
+          </div>
+          <div className="grid grid-cols-7 mt-10 gap-4">
+            <div>
+              <img
+                src={project.designs[1]}
+                className="w-48 h-48 object-cover mx-auto"
+              />
+              <p className="text-center">1890</p>
+            </div>
+            <div>
+              <img
+                src={project.designs[2]}
+                className="w-48 h-48 object-cover mx-auto"
+              />
+              <p className="text-center">1900</p>
+            </div>
+            <div>
+              <img
+                src={project.designs[3]}
+                className="w-48 h-48 object-cover mx-auto"
+              />
+              <p className="text-center">1900</p>
+            </div>
+            <div>
+              <img
+                src={project.designs[4]}
+                className="w-48 h-48 object-cover"
+              />
+              <p className="text-center">1910</p>
+            </div>
+            <div>
+              <img
+                src={project.designs[5]}
+                className="w-48 h-48 object-cover"
+              />
+              <p className="text-center">1910</p>
+            </div>
+            <div>
+              <img
+                src={project.designs[6]}
+                className="w-48 h-48 object-cover"
+              />
+              <p className="text-center">1929</p>
+            </div>
+            <div>
+              <img
+                src={project.designs[7]}
+                className="w-48 h-48 object-cover"
+              />
+              <p className="text-center">1940</p>
+            </div>
+          </div>
+          <p>
+            <br /> <br />
+            The design starts with a coffee shop, preserved as part of locals’
+            daily routine, and transitions through a mini museum into the
+            library. The library moves from open communal areas to intimate
+            underground zones, culminating in the hypocaust chamber, which
+            houses historical artifacts and rare books, merging personal memory
+            with collective history. Functions are arranged to follow the
+            bathhouse’s original spatial sequence from the cold (public coffee
+            shop), through the museum, to the hot (library), enhancing
+            circulation and experience. The main entrance is reused, other
+            entries closed, and original internal connections between rooms
+            restored. In addition, there is also a venue space with its own
+            entrance designated for events, it is separated from the rest of the
+            spaces, since it is not considered one of the main rooms in the
+            bathhouse, so we gave it an occasional function. There are two ways
+            to approach the underground space: the first using a spiral
+            staircase and the second using the back vault and into the old oven
+            space. It was important for us to give the people a chance to get
+            direct contact/ friction with underground archaeology and get the
+            full experience. The underground path functions both as an
+            experiential route and an archive, with shelves integrated between
+            the hypocaust columns to display and store special books and maps
+            within reach. Furniture was designed to fit seamlessly within curved
+            stone walls, columns, and niches, centered to maintain circulation,
+            while contrasting the stone with modern, lightweight pieces
+            featuring curved edges and thin black metal. Black metal is also
+            used in staircases and the building’s support system. Floor levels
+            were adjusted to create the path, and a steel beam and column grid
+            was carefully aligned with the hypocaust to avoid disturbing the
+            archaeology or circulation. In the hot room, we created a unique
+            experience by adding a central opening to reveal the underground
+            path and hypocaust system. The floating floor includes a gap to
+            preserve the stone walls and emphasize the connection between upper
+            and lower levels, enhanced by a light fixture radiating in both
+            directions. Light plays a key role in the spatial experience: bright
+            lighting in the coffee shop conveys openness, gradually dimming into
+            softer, intimate lighting deeper inside. The intervention respects
+            the exterior, keeping facade openings’ dimensions and grid ratios
+            consistent with the upper floors.
+          </p>
         </div>
         <div id="materials" className="pt-10">
-          <h2 className="text-2xl font-bold font-serif mb-4">Materials</h2>
+          <h2 className="text-2xl font-bold font-serif mb-10">Materials</h2>
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mt-5">
+            {project.materials.map((material, idx) => (
+              <img
+                key={idx}
+                src={material}
+                alt={`${project.title} Material ${idx + 1}`}
+                className="w-full h-auto object-cover rounded-lg"
+              />
+            ))}
+          </div>
         </div>
         <div id="renders" className="pt-10">
           <h2 className="text-2xl font-bold font-serif mb-4">Renders</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            {project.renders.map((render, idx) => (
+              <img
+                key={idx}
+                src={render}
+                alt={`${project.title} Render ${idx + 1}`}
+                className="w-full h-auto object-cover rounded-lg"
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
