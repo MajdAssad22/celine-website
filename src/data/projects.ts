bce28e076fd1a742596d8f28d7d65c5d0f883ab5
Majd Assad <majdassad123@gmail.com>
2025-11-28 11:57:05 -0500

    Update project assets and details for Hygeia and Ventusphere; add new designs and modify project attributes

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,14 +1,16 @@
-import hygeia from "@/assets/hygeia.png";
+// Hygeia import
+import hygeia from "@/assets/hygeia/hygeia-thumbnail.png";
+
+// Store & Story imports
+import storyAndStoneThumbnail from "@/assets/story-and-stone/story-and-stone-thumbnail.png";
 import storyAndStoneRender1 from "@/assets/story-and-stone/renders/story-and-stone-render-1.jpeg";
 import storyAndStoneRender2 from "@/assets/story-and-stone/renders/story-and-stone-render-2.jpeg";
 import storyAndStoneRender3 from "@/assets/story-and-stone/renders/story-and-stone-render-3.jpeg";
 import storyAndStoneRender4 from "@/assets/story-and-stone/renders/story-and-stone-render-4.jpeg";
-import storyAndStoneThumbnail from "@/assets/story-and-stone/story-and-stone-thumbnail.png";
 import storyAndStoneMaterial1 from "@/assets/story-and-stone/materials/story-and-stone-material-1.png";
 import storyAndStoneMaterial2 from "@/assets/story-and-stone/materials/story-and-stone-material-2.png";
 import storyAndStoneMaterial3 from "@/assets/story-and-stone/materials/story-and-stone-material-3.png";
 import storyAndStoneMaterial4 from "@/assets/story-and-stone/materials/story-and-stone-material-4.png";
-
 import storyAndStoneDesign1 from "@/assets/story-and-stone/designs/story-and-stone-design-1.jpeg";
 import storyAndStoneDesign2 from "@/assets/story-and-stone/designs/story-and-stone-design-2.jpeg";
 import storyAndStoneDesign3 from "@/assets/story-and-stone/designs/story-and-stone-design-3.jpeg";
@@ -17,7 +19,21 @@ import storyAndStoneDesign5 from "@/assets/story-and-stone/designs/story-and-sto
 import storyAndStoneDesign6 from "@/assets/story-and-stone/designs/story-and-stone-design-6.jpeg";
 import storyAndStoneDesign7 from "@/assets/story-and-stone/designs/story-and-stone-design-7.jpeg";
 import storyAndStoneDesign8 from "@/assets/story-and-stone/designs/story-and-stone-design-8.jpeg";
-import ventusphere from "@/assets/ventusphere.png";
+import storyAndStoneDesign9 from "@/assets/story-and-stone/designs/story-and-stone-design-9.jpeg";
+import storyAndStoneDesign10 from "@/assets/story-and-stone/designs/story-and-stone-design-10.jpeg";
+import storyAndStoneDesign11 from "@/assets/story-and-stone/designs/story-and-stone-design-11.jpeg";
+import storyAndStoneDesign12 from "@/assets/story-and-stone/designs/story-and-stone-design-12.jpeg";
+import storyAndStoneDesign13 from "@/assets/story-and-stone/designs/story-and-stone-design-13.jpeg";
+import storyAndStoneDesign14 from "@/assets/story-and-stone/designs/story-and-stone-design-14.jpeg";
+import storyAndStoneDesign15 from "@/assets/story-and-stone/designs/story-and-stone-design-15.jpeg";
+import storyAndStoneDesign16 from "@/assets/story-and-stone/designs/story-and-stone-design-16.jpeg";
+import storyAndStoneDesign17 from "@/assets/story-and-stone/designs/story-and-stone-design-17.jpeg";
+import storyAndStoneDesign18 from "@/assets/story-and-stone/designs/story-and-stone-design-18.jpeg";
+
+// Ventusphere import
+import ventusphereThumbnail from "@/assets/ventusphere/ventusphere-thumbnail.png";
+import ventusphereBanner from "@/assets/ventusphere/ventusphere-banner.jpeg";
+import ventusphereReneder1 from "@/assets/ventusphere/renders/ventusphere-render-1.png";
 
 export const projects = [
   {
@@ -25,6 +41,7 @@ export const projects = [
     slug: "stone-and-story",
     title: "Stone & Story",
     category: "Interior",
+    awardWinning: false,
     points: ["Culture - History", "Conservation", "Archeology", "Community"],
     concepts: [
       "Community",
@@ -33,16 +50,6 @@ export const projects = [
       "Cultural Center",
       "Heritage",
     ],
-    conceptExplanation:
-      "Stone & Story explores how heritage and history can be integrated into everyday life. As designers from Nazareth, we observed a gap in local awareness of our Arab culture and heritage, as most historical sites prioritize tourists over residents. We selected an Ancient Roman Bathhouse as the site for our intervention. Our proposal reimagines it as a public library, preserving the bathhouse’s original stone structure while introducing new elements. This approach balances respect for historical authenticity with the practical needs of the present. The result is more than a library; it is a living cultural hub. The design transforms the bathhouse into a space where locals encounter their history in daily life, blending memory, continuity, and community. Emphasizing accessibility, interaction, and continuity, the project ensures that history is not just observed but actively experienced, fostering a dialogue between past and future generations.",
-    research:
-      "We chose Nazareth for our project due to our personal connection, its 2,000-year history, cultural diversity, and significance as the childhood home of Jesus. To understand the city, we studied its layout, surrounding towns, roads, and cultural landscape. Focusing on what the word culture contains, we categorized it into" +
-      "heritage & traditions, social & family structure, language & literature, music & dance, and handcrafts & visual arts. Mapping key cultural sites revealed they are scattered with no unifying element, and the city has only one library, located far from the center. We also observed that most cultural centers emphasized artistry while overlooking historical depth." +
-      "This encouraged us to examine Nazareth’s timeline from the Ancient Roman period to the present, highlighting architectural features from each era. Field research revealed surviving elements in the city center, particularly underground archaeological sites such as the Basilica of the Annunciation, holy caves, and the ancient bathhouse. Exploring these sites showed us that many were unfamiliar to locals and primarily oriented toward tourists. This inspired us to integrate an archaeological site into daily public life. We chose the Ancient Roman Bathhouse to avoid religious sites and establish a space that reconnects locals with the city’s rich historical layers.",
-    design:
-      "We studied the Roman bathhouse’s original function, layout, and sequence of spaces from the changing room through cold, lukewarm, and hot rooms supported by an underfloor heating system. Today, the bathhouse is divided into separate spaces with individual entrances, and the original connections are closed. To understand the history of the building, we collected visual documents from 1890 to the present and analyzed original versus added stones, aiming to preserve the authentic structure while removing later interventions." +
-      "Our proposal envisions a public library that reconnects the people of Nazareth with their cultural and historical heritage. We sought to preserve the bathhouse’s original spatial logic, the sequence of hot, lukewarm, and cold rooms, and reimagined it as a narrative structure within the new program." +
-      "The design starts with a coffee shop, preserved as part of locals’ daily routine, and transitions through a mini museum into the library. The library moves from open communal areas to intimate underground zones, culminating in the hypocaust chamber, which houses historical artifacts and rare books, merging personal memory with collective history. Functions are arranged to follow the bathhouse’s original spatial sequence from the cold (public coffee shop), through the museum, to the hot (library), enhancing circulation and experience. The main entrance is reused, other entries closed, and original internal connections between rooms restored. In addition, there is also a venue space with its own entrance designated for events, it is separated from the rest of the spaces, since it is not considered one of the main rooms in the bathhouse, so we gave it an occasional function. It was important for us to give the people a chance to get direct contact/ friction with underground archaeology and get the full experience. There are two options to approach the underground space: the first using a spiral staircase and the second using the back vault and into the old oven space. The underground path functions both as an experiential route and an archive, with shelves integrated between hypocaust columns to display and store special books and maps within reach. Furniture was designed to fit seamlessly within curved stone walls, columns, and niches, centered to maintain circulation, while contrasting the stone with modern, lightweight pieces featuring curved edges and thin black metal. Black metal is also used in staircases and the building’s support system. Floor levels were adjusted to create the path, and a steel beam and column grid was carefully aligned with the hypocaust to avoid disturbing the archaeology or circulation. In the hot room, we created a unique experience by adding a central opening to reveal the underground path and hypocaust system. The floating floor includes a gap to preserve the stone walls and emphasize the connection between upper and lower levels, enhanced by a light fixture radiating in both directions. Light plays a key role in the spatial experience: bright lighting in the coffee shop conveys openness, gradually dimming into softer, intimate lighting deeper inside. The intervention respects the exterior, keeping facade openings’ dimensions and grid ratios consistent with the upper floors.",
     thumbnail: storyAndStoneThumbnail,
     banner: storyAndStoneRender1,
     renders: [
@@ -66,6 +73,16 @@ export const projects = [
       storyAndStoneDesign6,
       storyAndStoneDesign7,
       storyAndStoneDesign8,
+      storyAndStoneDesign9,
+      storyAndStoneDesign10,
+      storyAndStoneDesign11,
+      storyAndStoneDesign12,
+      storyAndStoneDesign13,
+      storyAndStoneDesign14,
+      storyAndStoneDesign15,
+      storyAndStoneDesign16,
+      storyAndStoneDesign17,
+      storyAndStoneDesign18,
     ],
   },
   {
@@ -73,6 +90,7 @@ export const projects = [
     slug: "hygeia",
     title: "Hygeia",
     category: "Interior",
+    awardWinning: true,
     points: [
       "Futuristic design",
       "Modularity",
@@ -93,14 +111,10 @@ export const projects = [
     slug: "ventusphere",
     title: "Ventusphere",
     category: "Interior",
-    points: ["Natural ventilation", "Clients"],
-    concepts: [
-      "Community",
-      "Daily Use",
-      "Archeology",
-      "Cultural Center",
-      "Heritage",
-    ],
-    thumbnail: ventusphere,
+    awardWinning: false,
+    points: ["Natural ventilation", "Raw materials", "Client focused"],
+    concepts: ["Natural ventilation", "Raw materials", "Client focused"],
+    thumbnail: ventusphereThumbnail,
+    banner: ventusphereBanner,
   },
 ];
